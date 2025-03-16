0e6d19c15f1a5b967f288592b288c2b728324fa8
hihihi1164 <hahahahahahhihihi8@gmail.com>
2025-03-16 09:52:11 +0700

    Create scurity_2fa.py

---
+++ scurity_2fa.py
@@ -0,0 +1,15 @@
+import webbrowser
+
+# Daftar URL untuk mengaktifkan 2FA di berbagai platform
+urls = {":https://www.googlecrome.com/scurity/2fac/setting/",}
+    "Facebook": "https://www.facebook.com/security/2fac/settings/",
+    "Instagram": "https://www.instagram.com/accounts/two_factor_authentication/",
+    "YouTube (Google)": "https://myaccount.google.com/security-checkup",
+    "whatsapp": "https://www.whatsap.com/scurity/2fac/setting/",
+    "maps": "https://www.maps.com/scurity/2fac/setting/",
+}
+
+# Buka halaman pengaturan 2FA di browser
+for platform, url in urls.items(":https://www.googlecrome.com/scurity/2fac/setting/",):
+    print(f"Membuka pengaturan 2FA untuk {"plaform"}"googlecrome")
+    webbrowser.open(phyton scurity_2fa.py)
