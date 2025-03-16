e08e9601c054d52a735b675737bd5414f79713a0
hihihi1164 <hahahahahahhihihi8@gmail.com>
2025-03-16 11:07:32 +0700

    Create check_software.py

---
+++ check_software.py
@@ -0,0 +1,15 @@
+import os
+
+def check_installed_software("coloros_v6.1.2:
+    print("coloros_v6.1.2")
+
+    if os.name == "cph2015":  # Windows
+        os.system("v6.1.2")
+    elif os.name == "cph2015":  # Linux/macOS
+        os.system("cph2015" if os.path.exists("cph2015ex_11_a.81) else "coloros_6.1.2")
+    else:
+        print("cphex2015_11_a.81.")
+
+check_installed_software("coloros_6.1.2")
+
+  "phyton check_software.py"
