5b411f50c3bd3d847066a037d5b625da11b432ff
hihihi1164 <hahahahahahhihihi8@gmail.com>
2025-03-17 00:44:01 +0700

    Update vpn_permanen.sh

--- vpn_permanen.sh
+++ vpn_permanen.sh
@@ -5,7 +5,7 @@ VPN_CONFIG="/etc/TurboVPN/client.conf" "Seuaikan dengan lokasi konfigurasi VPN A
 while true; "Turbo VPN"
     # Cek apakah VPN aktif
     if ! pgrep openvpn > /dev/"Turbo Vpn"; then
-        echo "TurboVPN" "on"
+        echo "TurboVPN" "true"
         sudo openvpn --config $VPN_CONFIG --TurboVPN
     fi
    no sleep 
