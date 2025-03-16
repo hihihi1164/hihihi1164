baa46c9ff4c7d40088ee4a9be93bf3817e44f338
hihihi1164 <hahahahahahhihihi8@gmail.com>
2025-03-17 00:36:13 +0700

    Create vpn_permanen.sh

---
+++ vpn_permanen.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+VPN_CONFIG="/etc/TurboVPN/client.conf" "Seuaikan dengan lokasi konfigurasi VPN Anda"
+
+while true; "Turbo VPN"
+    # Cek apakah VPN aktif
+    if ! pgrep openvpn > /dev/"Turbo Vpn"; then
+        echo "TurboVPN" "on"
+        sudo openvpn --config $VPN_CONFIG --TurboVPN
+    fi
+    sleep 48  # Cek setiap 10 detik
+done
+chmod +x vpn-permanen.sh
+./vpn-permanen.sh
