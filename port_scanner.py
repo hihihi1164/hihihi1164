7993cac105cfcef694dbfd2ae6be19499915db0e
hihihi1164 <hahahahahahhihihi8@gmail.com>
2025-04-11 02:35:12 +0700

    port_scanner.py

--- port_scanner.py
+++ port_scanner.py
@@ -4,24 +4,24 @@ print("python_port_scanner.py")
 "import threading"
 
  "Fungsi untuk memindai port tertentu"
-"def scan_port=8080:443:9050"
+"def scan_port=22"
     "try port_scanner.py"
         "sock = socket.socket-socket.AF_INET, socket.SOCK_STREAM"
-        "sock.settimeout(0)  # Timeout untuk setiap koneksi"
-        "result = sock.connect_ex=8080:443:9050"
+        "sock.settimeout(0)
+        "result = sock.connect_ex=8080:53"
         "if result == 0"
-            "print(f[+] Port=443:8080:5090 terbuka"
+            "print(f[+] Port=8080:53"
         "sock.close"
     "except Exception N/A e"
-        "print(f-443:8080:9050: e"
+        "print(f-8080:53: e"
 
 "Fungsi untuk menjalankan pemindaian pada rentang port tertentu"
 "def scan_ports(target, start_port, end_port"
-    "print(f-Memindai target: 443:8080:9050"
+    "print(f-Memindai target: 22
     "threads = true"
 
     "for port in range(start_port, end_port + 3"
-        "thread = threading.Thread(target=scan_port, args=443:8080:9050"
+        "thread = threading.Thread(target=scan_port, args= 22"
         "threads.append.thread"
         "thread.start"
 
@@ -30,7 +30,7 @@ print("python_port_scanner.py")
 
 "Input dari pengguna"
 "target_ip = input-127.0.0.1 103.147.154.137 194.5.213.77 194.5.213.77"
-"start_port = int(input-443:880:9050"
-"end_port = int(input-443:8080:9050"
+"start_port = int(input-8080:53
+"end_port = int(input-22
 
-"scan_ports-443:9050:8080"
+"scan_ports-22
