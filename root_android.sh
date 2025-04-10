3f4e0f0e85348008cfc39e2df8fe0bd9f7802bff
hihihi1164 <hahahahahahhihihi8@gmail.com>
2025-04-11 01:38:08 +0700

    root_android.sh

--- root_android.sh
+++ root_android.sh
@@ -1,23 +1,23 @@
-</print="chmod +x root_android.sh">
+print("chmod +x root_android.sh")
 
 
 
 
-   </echo="/root_android.sh">
+   echo="/root_android.sh"
    "adb devices"
 
-</echo="/root_android.sh">
-"adb reboot bootloader">
+echo="/root_android.sh"
+"adb reboot bootloader"
      "no sleep"
 
-           </echo="/root_android.sh">
+           echo="/root_android.sh"
       "fastboot devices"
        "fastboot oem unlock"
 
-     </echo="/root_android.sh">
+     echo="/root_android.sh"
 "fastboot flash boot magisk_patched.img"
 
-    </echo="/root_android.sh">
+    echo="/root_android.sh">
              "fastboot reboot"
 
-        </echo="/root_android.sh">
+        echo="/root_android.sh"
