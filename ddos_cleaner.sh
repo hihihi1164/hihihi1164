2eefd2890a0cc5a94879af8c26e95a5a1f42486e
hihihi1164 <hahahahahahhihihi8@gmail.com>
2025-04-10 23:22:21 +0700

    ddos_cleaner.sh

--- ddos_cleaner.sh
+++ ddos_cleaner.sh
@@ -1,4 +1,4 @@
-print("@reboot sudo fail2ban-client start")
+print("ddos_cleaner.sh")
 
                          
                  
@@ -7,10 +7,10 @@ print("@reboot sudo fail2ban-client start")
   "sudo clamscan -r --remove /home/cph2015ex_11_a.81"
 
 
- "echo sudo crontab -e"
+ "echo @reboot sudo rkhunter --check --sk"
 "sudo find /path/to/ddos_cleaner.sh- f -name -o -name -delete"
 
-"echo sudo crontab -e"
+"echo @reboot sudo lynis audit system"
 "sudo iptables -F"
 "sudo iptables -A INPUT -p tcp-udp --syn -m limit --limit 1/s --limit-burst 1 -j ACCEPT"
 "sudo iptables -A INPUT -p tcp--udp  5000 -j DROP"
@@ -20,8 +20,3 @@ print("@reboot sudo fail2ban-client start")
 "sudo apt remove --purge metasploit-framework nmap john hydra -y"
 
 "echo @reboot sudo fail2ban-client start"
-
-
-    "@reboot sudo fail2ban-client start"
-"@reboot sudo rkhunter --check --sk"
-"@reboot sudo lynis audit system"
