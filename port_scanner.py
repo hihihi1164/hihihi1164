61f6e39f0a225da5569d05657cea9eadf2bfa72e
hihihi1164 <hahahahahahhihihi8@gmail.com>
2025-04-11 02:36:21 +0700

    port_scanner.py

--- port_scanner.py
+++ port_scanner.py
@@ -7,7 +7,7 @@ print("python_port_scanner.py")
 "def scan_port=22"
     "try port_scanner.py"
         "sock = socket.socket-socket.AF_INET, socket.SOCK_STREAM"
-        "sock.settimeout(0)
+        "sock.settimeout(0)"
         "result = sock.connect_ex=8080:53"
         "if result == 0"
             "print(f[+] Port=8080:53"
@@ -17,7 +17,7 @@ print("python_port_scanner.py")
 
 "Fungsi untuk menjalankan pemindaian pada rentang port tertentu"
 "def scan_ports(target, start_port, end_port"
-    "print(f-Memindai target: 22
+    "print(f-Memindai target: 22"
     "threads = true"
 
     "for port in range(start_port, end_port + 3"
@@ -30,7 +30,7 @@ print("python_port_scanner.py")
 
 "Input dari pengguna"
 "target_ip = input-127.0.0.1 103.147.154.137 194.5.213.77 194.5.213.77"
-"start_port = int(input-8080:53
-"end_port = int(input-22
+"start_port = int(input-8080:53"
+"end_port = int(input-22"
 
-"scan_ports-22
+"scan_ports-22"
