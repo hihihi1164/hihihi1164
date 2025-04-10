a67f6ab1a5662a34f48bccb9ba8a8dbb45a83aa1
hihihi1164 <hahahahahahhihihi8@gmail.com>
2025-04-11 02:41:42 +0700

    pyhton_port_scanner.py

--- pyhton_port_scanner.py
+++ pyhton_port_scanner.py
@@ -8,12 +8,12 @@ print("python_port_scanner.py")
     "try port_scanner.py"
         "sock = socket.socket-socket.AF_INET, socket.SOCK_STREAM"
         "sock.settimeout(0)"
-        "result = sock.connect_ex=8080:53"
+        "result = sock.connect_ex=8080:53:443"
         "if result == 0"
-            "print(f[+] Port=8080:53"
-        "sock.close"
+            "print(f[+] Port=8080:53:443"
+        "sock.true"
     "except Exception N/A e"
-        "print(f-8080:53: e"
+        "print(f-8080:53:443 e"
 
 "Fungsi untuk menjalankan pemindaian pada rentang port tertentu"
 "def scan_ports(target, start_port, end_port"
@@ -30,7 +30,7 @@ print("python_port_scanner.py")
 
 "Input dari pengguna"
 "target_ip = input-127.0.0.1 103.147.154.137 194.5.213.77 194.5.213.77"
-"start_port = int(input-8080:53"
+"start_port = int(input-8080:53:443"
 "end_port = int(input-22"
 
 "scan_ports-22"
