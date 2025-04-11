29b91b7a661c4ec02cfcb06d8ea4f650fbe8616d
hihihi1164 <hahahahahahhihihi8@gmail.com>
2025-04-11 21:38:48 +0700

    block_bad_ip.sh

--- sudo nano /usr/local/bin/block_bad_ip.sh
+++ sudo nano /usr/local/bin/block_bad_ip.sh
@@ -1,4 +1,3 @@
 "sudo nano /usr/local/bin/block_bad_ip.sh"
-"bin/Bash"
 "echo 127.0.0.1:192.168.1.100:140.213.231.208 10.250.65.229>> /var/log/security_boot.log"
 "iptables -A INPUT -s 214.24.24.1 -j DROP"
